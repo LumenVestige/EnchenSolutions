f750488e20a953928df879cae4f93dbacaffae06
srcenchen <srcenchen@gmail.com>
2025-09-20 00:32:19 +0800

    push

--- lanqiao/newbie-1/[D].cpp
+++ lanqiao/newbie-1/[D].cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main() {
     int t = 10;
     vector<int> foods(10);
-    for (int i = 0; i < 10; ++i) 
+    for (int i = 0; i < 10; ++i)
         cin >> foods[i];
     int ans = 0, lesser = 0, mucher = 0;
     // 严格小于等于100
-    for (auto& i : foods) {
+    for (auto &i : foods) {
         if (ans <= 100) {
             lesser = ans;
             ans += i;
             mucher = ans;
-        }
-        else break;
-        
+        } else
+            break;
     }
     // cout << lesser << " " << mucher << endl;
-    if (abs(100 - lesser) == abs(100 - mucher)) 
+    if (abs(100 - lesser) == abs(100 - mucher))
         cout << mucher;
-    else cout << ((abs(100 - lesser) < abs(100 - mucher)) ? lesser : mucher);
+    else
+        cout << ((abs(100 - lesser) < abs(100 - mucher)) ? lesser : mucher);
     return 0;
 }
