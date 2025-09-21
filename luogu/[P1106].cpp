bfc0cc5bd4f3cb0590a4b24731da8669b9ab2f40
sanenchen <srcenchen@gmail.com>
2025-09-21 16:37:10 +0800

    push

--- luogu/[P1106].cpp
+++ luogu/[P1106].cpp
@@ -3,19 +3,30 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-char findMin(string s) {
-    char m = 'A';
-    for (auto& i : s) {
-        m = min(m, i);
-    }
-    return m;
-}
 int main() {
     string s;
     int k;
     cin >> s >> k;
-    for (int i = 0; i < s.size(); ++i) {
-        if (k)
+    while (k--) {
+        for (int i = 0; i < s.size(); ++i) {
+            // 找到下降点
+            // 当前的这个数大于后一个数 则删除这个数
+            if (i < s.size() - 1 && s[i] > s[i + 1]) {
+                s.erase(s.begin() + i);
+                break;
+            }
+            if (i == s.size() - 1) {
+                s.erase(s.begin() + i);
+            }
+        }
     }
+    // 去除前导0
+    int p = 0;
+    while (p != 0) {
+        if (s[p] == '0') {
+            s.erase(s.begin());
+        }
+    }
+    cout << s;
     return 0;
 }
