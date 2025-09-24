fec3527291ecdc15b6e791ce3c8f2faaf5e8ae7c
sanenchen <srcenchen@gmail.com>
2025-09-24 19:20:00 +0800

    push

--- leetcode/[402]-removeKdigits.cpp
+++ leetcode/[402]-removeKdigits.cpp
@@ -4,51 +4,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 string removeKdigits(string num, int k) {
-    // stack<int> st;
-    // vector<int> downPos(num.size(), -1);
-    // for (int i = num.size() - 1; i >= 0; --i) {
-    //     while (!st.empty() && num[i] <= num[st.top()])
-    //         st.pop();
-    //     if (!st.empty())
-    //         downPos[i] = st.top();
-    //     st.push(i);
-    // }
-    // // 开删！
-    // string ans;
-    // for (int i = 0; i < downPos.size(); ++i) {
-    //     if (downPos[i] == i + 1 && k > 0) {
-    //         k--;
-    //         continue;
-    //     } else {
-    //         ans += num[i];
-    //     }
-    // }
-    // while (k > 0) {
-    //     ans.erase(ans.end() - 1);
-    //     k--;
-    // }
-    string s = num;
-    while (k--) {
-        for (int i = 0; i < s.size(); ++i) {
-            // 找到下降点
-            // 当前的这个数大于后一个数 则删除这个数
-            if (i < s.size() - 1 && s[i] > s[i + 1]) {
-                s.erase(s.begin() + i);
-                break;
-            }
-            if (i == s.size() - 1) {
-                s.erase(s.begin() + i);
-            }
+    stack<char> st;
+    for (char i : num) {
+        // 如果当前的数 大于了栈 则退栈
+        while (!st.empty() && i < st.top() && k > 0) {
+            st.pop();
+            k--;
         }
+        st.push(i);
     }
-    // 去除前导0
-    while (1) {
-        if (s[0] != '0') break;
-        s = s.substr(1, s.size() - 1);
+    // 栈转字符串
+    string ans;
+    while (!st.empty()) {
+        ans += st.top();
+        st.pop();
     }
-    return s.empty() ? "0" : s;
+    reverse(ans.begin(), ans.end());
+    // 删除最后一位 如果k大于0
+    while (k > 0) {
+        ans.erase(ans.end() - 1);
+        k--;
+    }
+    while (!ans.empty() && ans[0] == '0')
+        ans.erase(ans.begin()); // 删除前导0
+    if (ans.empty()) return "0";
+    return ans;
 }
 int main() {
-    cout << removeKdigits("33526221184202197273", 19);
+    cout << removeKdigits("1432219", 3);
     return 0;
 }
