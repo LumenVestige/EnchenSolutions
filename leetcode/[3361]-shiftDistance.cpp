eafb03e7d6113061187af8ed5f325dbea76a13e1
sanenchen <srcenchen@gmail.com>
2025-09-26 16:53:57 +0800

    push

--- leetcode/[3361]-shiftDistance.cpp
+++ leetcode/[3361]-shiftDistance.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 long long shiftDistance(string s, string t, vector<int>& nextCost, vector<int>& previousCost) {
-    vector<int> nextCostPrefix(nextCost.size() + 1, 0), previousCostPrefix(previousCost.size() + 1, 0);
+    vector<long long> nextCostPrefix(nextCost.size() + 1, 0), previousCostPrefix(previousCost.size() + 1, 0);
     for (int i = 1; i <= nextCost.size(); ++i) 
         nextCostPrefix[i] = nextCostPrefix[i - 1] + nextCost[i - 1];
     for (int i = 1; i <= previousCost.size(); ++i) 
@@ -13,20 +13,23 @@ long long shiftDistance(string s, string t, vector<int>& nextCost, vector<int>& 
         // int pa = nextCostPrefix[26 - diff - 1];
         // int pb = previousCostPrefix[diff - 1];
         // ans += min(pa, pb);
-        int pa = 0;
+        long long pa = 0;
         // 向后处理
         if (s[i] < t[i]) {
             pa = nextCostPrefix[t[i] - 'a'] - nextCostPrefix[s[i] - 'a'];
         } else {
-            pa = nextCostPrefix[nextCostPrefix.size() - 1] - (nextCostPrefix[t[i] - 'a'] - nextCostPrefix[s[i] - 'a']);
+            pa = nextCostPrefix[nextCostPrefix.size() - 1] - nextCostPrefix[s[i] - 'a'] + nextCostPrefix[t[i] - 'a'];
         }
-        int pb = 0;
+        long long pb = 0;
         if (s[i] < t[i]) {
-            pb = previousCostPrefix[t[i] - 'a'] - previousCostPrefix[s[i] - 'a'];
+            pb = previousCostPrefix[s[i] - 'a' + 1] + previousCostPrefix[previousCostPrefix.size() - 1] 
+             - previousCostPrefix[t[i] - 'a' + 1];
         } else {
-            pb = previousCostPrefix[previousCostPrefix.size() - 1] - (previousCostPrefix[t[i] - 'a'] - previousCostPrefix[s[i] - 'a']);
+            pb = previousCostPrefix[t[i] - 'a' + 1] - previousCostPrefix[s[i] - 'a' + 1];
         }
-        ans += min(pa, pb);
+        // ans += min(pa, pb);
+        //cout << min(pa, pb) << endl;
+        ans += min(abs(pa), abs(pb));
     }
     return ans;
 }
