fd8945a5c22c44a0df20e1c39100b324ea3cf590
= <srcenchen@gmail.com>
2025-11-10 19:57:51 +0800

    commit

---
+++ codeforces/[2167]-C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
