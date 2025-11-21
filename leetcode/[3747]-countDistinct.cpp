06a6aea999c7ddeaff1dfbee91d4d376842d7821
sanenchen <srcenchen@gmail.com>
2025-11-22 02:11:30 +0800

    commit

--- leetcode/[3747]-countDistinct.cpp
+++ leetcode/[3747]-countDistinct.cpp
@@ -1,37 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-// 能力不够，卡三天了。。。
-// 先跳过了
+using ll = long long;
 long long countDistinct(long long n) {
     string s = to_string(n);
-    int m = s.size();
-
-    // 计算长度小于 m 的不含 0 的整数个数
-    // 9^1 + 9^2 + ... + 9^(m-1) = (9^m - 9) / 8
-    long long pow9 = pow(9, m);
-    long long ans = (pow9 - 9) / 8;
-
-    // 计算长度恰好等于 m 的不含 0 的整数个数
-    for (int i = 0; i < m; i++) {
-        char d = s[i];
-        if (d == '0') { // 只能填 0，不合法，跳出循环
-            break;
+    // 记忆化搜索
+    ll memo[18][2][2]; // 记录状态用的 pos isLimit isLead
+    memset(memo, -1, sizeof(memo));
+    auto dfs = [&](auto&& self, int pos, bool isLimit, bool isLead) -> ll {
+        // 终止条件：如果pos == s.size 
+        if (pos == s.size()) {
+            return isLead ? 0 : 1;
         }
-        // 这一位填 1 到 d-1，后面的数位可以随便填 1 到 9
-        int v = d - '1';
-        if (i == m - 1) {
-            v++; // 最低位可以等于 d
+        // 记忆化搜索 查表
+        if (memo[pos][isLimit][isLead] != -1) {
+            return memo[pos][isLimit][isLead];
         }
-        pow9 /= 9;
-        ans += v * pow9;
-        // 然后，这一位填 d，继续遍历
-    }
 
-    return ans;
+        // 标记 up 标签, 需要检查当前是否是在限制模式下
+        int up = isLimit ? s[pos] - '0' : 9;
+        ll res = 0;
+        for (int i = 0; i <= up; ++i) {
+            // 前导0判断
+            if (isLead) {
+                // 延续前导0状态
+                res += self(self, pos + 1, isLimit && (i == up), i == 0); // 只有当当前的数字为0才会允许延续前导0状态
+            } else {
+                if (i != 0) {
+                    res += self(self, pos + 1, isLimit && (i == up), false);
+                }
+            }
+        }
+        memo[pos][isLimit][isLead] = res; // 记忆化表设置
+        return res;
+    };
+    return dfs(dfs, 0, true, true);
 }
 
 int main() {
-    cout << countDistinct(1000);
+    cout << countDistinct(10000);
     return 0;
 }
