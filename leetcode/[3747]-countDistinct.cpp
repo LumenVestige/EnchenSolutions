1ab2c590ee7842cc62eeab1a28a651874a6ba405
sanenchen <srcenchen@gmail.com>
2025-11-22 00:56:52 +0800

    push

--- leetcode/[3747]-countDistinct.cpp
+++ leetcode/[3747]-countDistinct.cpp
@@ -1,31 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+using ll = long long;
+// 能力不够，卡三天了。。。
+// 先跳过了
 long long countDistinct(long long n) {
-    if (n < 10) {
-        return n;
-    }
-    // long long to str
-    string strNum;
-    while (n != 0) {
-        strNum += n % 10 + '0';
-        n /= 10;
-    }
-    reverse(strNum.begin(), strNum.end());
-    long long ans = 0;
-    for (int i = 0; i < strNum.size(); ++i) {
-        if (strNum[i] == '0') {
-            return ans;
+    string s = to_string(n);
+    int m = s.size();
+
+    // 计算长度小于 m 的不含 0 的整数个数
+    // 9^1 + 9^2 + ... + 9^(m-1) = (9^m - 9) / 8
+    long long pow9 = pow(9, m);
+    long long ans = (pow9 - 9) / 8;
+
+    // 计算长度恰好等于 m 的不含 0 的整数个数
+    for (int i = 0; i < m; i++) {
+        char d = s[i];
+        if (d == '0') { // 只能填 0，不合法，跳出循环
+            break;
         }
-        int tmpN = (strNum.size() - i - 1);
-        if (tmpN >= 2) ans += 9;
-        ans += pow(9, (strNum.size() - i - 1)) * (strNum[i] - '0');
-        //if (n >= 100)
+        // 这一位填 1 到 d-1，后面的数位可以随便填 1 到 9
+        int v = d - '1';
+        if (i == m - 1) {
+            v++; // 最低位可以等于 d
+        }
+        pow9 /= 9;
+        ans += v * pow9;
+        // 然后，这一位填 d，继续遍历
     }
+
     return ans;
 }
 
 int main() {
-    cout << countDistinct(2048);
+    cout << countDistinct(1000);
     return 0;
 }
