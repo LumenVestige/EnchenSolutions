78c8e1422a2402e386fa44694e85ed609810dfdf
sanenchen <srcenchen@gmail.com>
2025-11-27 22:18:19 +0800

    commit

--- cwxu-c/homework.cpp
+++ cwxu-c/homework.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int a;
-    cin >> a;
-    cout << a;
-    cout << "Hello World!" << endl;
+#include <stdio.h>
+  #include <stdlib.h>
+  
+  int main() {
+    int n;
+    scanf("%d", &n);
+    double ans = 1;
+    for (int i = 2; i <= n; ++i) {
+      ans -= 1.0 / i;
+    }
+    printf("%.5lf", ans);
     return 0;
-}
+  }
