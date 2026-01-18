9d8aa9d4b94c18cc99e32ac687d52dfbd8b10163
sanenchen <srcenchen@gmail.com>
2026-01-18 22:11:00 +0800

    commit

--- cwxu-c/homework.cpp
+++ cwxu-c/homework.cpp
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void sort(int* nums) {
+    // 冒泡排序
+    for (int i = 0; i < 10; ++i) {
+        for (int j = i; j < 10; ++j) {
+            if (nums[i] > nums[j]) {
+                nums[i] = nums[i] ^ nums[j];
+                nums[j] = nums[i] ^ nums[j];
+                nums[i] = nums[i] ^ nums[j];
+            }
+        }
+    }
+}
 int main() {
-    int i, *p;
-    scanf("%d", &i);
-    p = &i;
-    *p = 5;
-    printf("%d\n", i);
-    printf("%d\n", *p);
-    printf("%d\n", p);
-    printf("%d", &i);
+    int nums[10] = {2,4,2,5,6,1,33,44,22,989};
+    sort(nums);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", nums[i]);
+    }
     return 0;
 }
