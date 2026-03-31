e7809319aff3b740e0097ab014d07b33239d366b
sanenchen <srcenchen@gmail.com>
2026-03-31 16:47:02 +0800

    commit

---
+++ nowcoder/daily/[26-3-31].cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, a, b;
+    cin >> b >> a >> b;
+
+    return 0;
+}
