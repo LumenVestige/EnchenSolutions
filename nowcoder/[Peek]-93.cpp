9ad7944293421a20e15ede76366d648e838d1fe8
sanenchen <srcenchen@gmail.com>
2026-03-31 14:42:50 +0800

    commit

--- nowcoder/[Peek]-93.cpp
+++ nowcoder/[Peek]-93.cpp
@@ -2,34 +2,41 @@
 using namespace std;
 #define int long long
 #define endl "\n"
-
+// 最小生成树 Prim 算法
 signed main() {
-    int n, m, s;
-    cin >> n >> m >> s;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
     vector<vector<pair<int, int>>> grids(n+1);
     for (int i = 0; i < m; ++i) {
         int u, v, w;
         cin >> u >> v >> w;
         grids[u].push_back({v, w});
+        grids[v].push_back({u, w});
     }
-    vector<int> dis(n+1, LLONG_MAX), vis(n+1, 0);
+    int ans = 0;
+    int t = 0;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    dis[s] = 0;
-    pq.push({0, s});
+    vector<int> vis(n+1, 0);
+    pq.push({0, 1});
     while (!pq.empty()) {
         auto [w, v] = pq.top();
         pq.pop();
         if (vis[v]) continue;
-        for (auto [tv, tw] : grids[v]) {
-            if (dis[v] + tw < dis[tv]) {
-                dis[tv] = dis[v] + tw;
-                pq.push({dis[tv], tv});
+        for (auto [tV, tW] : grids[v]) {
+            if (!vis[tV]) {
+                pq.push({tW, tV});
             }
         }
+        ans += w;
+        t++;
         vis[v] = 1;
     }
-    for (int i = 1; i <= n; ++i) {
-        cout << (dis[i] == LLONG_MAX ? -1 : dis[i]) << " ";
+    if (t != n) {
+        cout << "NO";
+        return 0;
     }
+    cout << ans;
     return 0;
 }
